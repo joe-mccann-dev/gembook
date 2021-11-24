6054b98ad8879e0f081e29e442e93d44370703cd
Tenacious-Qi <joe.mccann@mailbox.org>
2021-11-24 16:41:37 -0500

    Setup basic user associations and controller actions.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts
+  has_many :comments
+  has_many :likes
+  has_many :liked_posts, through: :likes, source: :posts
+  has_one :profile
+  has_many :friendships
+  has_many :friends, -> { where(friendship: { status: 'accepted' }) }, through: :friendships, class_name: 'User'
+  has_many :friend_requests, -> { where(friendship: { status: 'pending' }) }, through: :friendships
+  has_many :friend_requests, -> { where(friendship: { status: 'denied' }) }, through: :friendships
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
+  resources :users, only: [:index, :show]
   # change this
   # root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
