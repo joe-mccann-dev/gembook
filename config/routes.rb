7ecb2daaa5f458f6c2fd3ad771e78bd9cf6f63e9
Tenacious-Qi <joe.mccann@mailbox.org>
2021-11-24 20:21:10 -0500

    Add sign in/out links to nav. Temporarily make users#index the root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   devise_for :users
   resources :users, only: [:index, :show]
-  # change this
-  # root to: "home#index"
+  root to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
