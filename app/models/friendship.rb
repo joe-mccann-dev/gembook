98e4cbdc83ee2bdace885d36deadee2674f97d6a
Tenacious-Qi <joe.mccann@mailbox.org>
2021-11-27 02:48:39 -0500

    Begin refactor of user-friendship association.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,9 @@
 class Friendship < ApplicationRecord
   enum status: %i[pending accepted declined]
 
-  belongs_to :friendable, polymorphic: true
+  belongs_to :sender, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
+
+  scope :friendship_accepted, -> { where(status: :accepted) }
+  scope :friendship_pending, -> { where(status: :pending) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,5 +8,27 @@ class User < ApplicationRecord
   has_many :likes
   has_many :liked_posts, through: :likes, source: :posts
   has_one :profile
+  # the one requesting the friendship is the 'sender', -> foreign_key is sender_id
+  has_many :sent_friend_requests, -> { friendship_pending },
+           class_name: 'Friendship',
+           foreign_key: 'sender_id'
+  # the one accepting the friendship is the 'receiver', -> foreign_key is receiver_id
+  has_many :received_friend_requests, -> { friendship_pending },
+           class_name: 'Friendship',
+           foreign_key: 'receiver_id'
 
+  has_many :accepted_sent_requests
+  has_many :accepted_received_requests
+  # the receivers of a user's friend requests are friends once status is 'accepted'
+  has_many :requested_friends,
+           through: :sent_friend_requests,
+           source: :receiver
+  # those who sent a user friend requests are friends once status is 'accepted'
+  has_many :received_friends,
+           through: :received_friend_requests,
+           source: :sender
+
+  def friends
+    requested_friends + received_friends
+  end
 end
