d9950a033f5c94328c8a9e4640ae3a3915d0d450
Tenacious-Qi <joe.mccann@mailbox.org>
2021-11-27 00:42:55 -0500

    revert db back to schema without user_id in friendships table.

--- db/migrate/20211125164341_add_user_id_to_friendships.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToFriendships < ActiveRecord::Migration[6.1]
-  def change
-    add_column :friendships, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_164341) do
+ActiveRecord::Schema.define(version: 2021_11_25_012502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2021_11_25_164341) do
     t.integer "receiver_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.index ["receiver_id"], name: "index_friendships_on_receiver_id"
     t.index ["sender_id", "receiver_id"], name: "index_friendships_on_sender_id_and_receiver_id", unique: true
     t.index ["sender_id"], name: "index_friendships_on_sender_id"
