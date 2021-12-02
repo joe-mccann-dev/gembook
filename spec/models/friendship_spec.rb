429e4fbb8b749f2634b78c286aefa29618a9d30c
Tenacious-Qi <joe.mccann@mailbox.org>
2021-12-01 19:45:24 -0500

    Write friendship and notification association specs.

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    Rails.application.load_seed
+  end
+  
+  let(:sender) { User.first }
+  let(:receiver) { User.create(first_name: 'friendship', last_name: 'receiver', email: 'foo@bar.com', password: 'foobar') }
+
+  it 'belongs to the sender of the request' do
+    friendship = Friendship.create(sender: sender, receiver: receiver)
+    expect(friendship.sender).to eq(sender)
+  end
+
+  it 'belongs to the receiver of the request' do
+    friendship = Friendship.create(sender: sender, receiver: receiver)
+    expect(friendship.receiver).to eq(receiver)
+  end
+
+  it 'has a default status of pending' do
+    friendship = Friendship.create(sender: sender, receiver: receiver)
+    expect(friendship.status).to eq('pending')
+  end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    Rails.application.load_seed
+  end
+
+  let(:sender) { User.first }
+  let(:receiver) { User.create(first_name: 'friendship', last_name: 'receiver', email: 'foo@bar.com', password: 'foobar') }
+
+  it 'belongs to the receiver of the notification' do
+    notification = sender.sent_notifications.create(receiver_id: receiver.id, object_type: 'Friendship', description: 'new friend request', time_sent: Time.zone.now)
+    expect(notification.sender).to eq(sender)
+  end
+
+  it 'belongs to the sender of the notification' do
+    notification = sender.sent_notifications.create(receiver_id: receiver.id, object_type: 'Friendship', description: 'new friend request', time_sent: Time.zone.now)
+    expect(notification.receiver).to eq(receiver)
+  end
+
+  it 'has a default read boolean set to false' do
+    notification = sender.sent_notifications.create(receiver_id: receiver.id, object_type: 'Friendship', description: 'new friend request', time_sent: Time.zone.now)
+    expect(notification.read).to eq(false)
+  end
 end
