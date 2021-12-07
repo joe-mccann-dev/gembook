6305b57332bd681055b0babae7030a34d6467077
Tenacious-Qi <joe.mccann@mailbox.org>
2021-12-07 02:07:04 -0500

    Add replies association to Comment.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
-  # has_many :replies, class_name: 'Comment', as: :commentable
+  has_many :replies, class_name: 'Comment', as: :commentable
+
   has_many :likes, as: :likeable
   has_many :likers, through: :likes, source: :user
 end
