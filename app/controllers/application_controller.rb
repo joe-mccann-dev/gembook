08d89d8dc801d493389c1c035345569b0051b235
Tenacious-Qi <joe.mccann@mailbox.org>
2021-12-29 12:15:53 -0500

    Fix unpermitted params in RegistrationsController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    attributes = [:first_name, :last_name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
 
   private
