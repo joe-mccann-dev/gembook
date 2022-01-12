3c712ccb4e37f1c1a868dc221cbbf8e7e179542f
Tenacious-Qi <joe.mccann@mailbox.org>
2022-01-12 00:22:44 -0500

    Modify db/seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-joe = User.create(first_name: 'Foo', last_name: 'Bar', email: 'joe.mccann@mailbox.org', password: 'foobar')
+foo = User.create(first_name: 'Foo', last_name: 'Bar', email: 'foo@bar.com', password: 'foobar')
 john = User.create(first_name: 'John', last_name: 'Smith', email: 'john@smith.com', password: 'foobar')
 jane = User.create(first_name: 'Jane', last_name: 'Doe', email: 'jane@doe.com', password: 'foobar')
 bob = User.create(first_name: 'Bob', last_name: 'Barker', email: 'bob@barker.com', password: 'foobar')
 ziggy = User.create(first_name: 'Ziggy', last_name: 'Stardust', email: 'ziggy@stardust.com', password: 'foobar')
 
-users = [joe, john, jane, bob, ziggy]
+users = [foo, john, jane, bob, ziggy]
 
-users.reject { |u| u == joe || u == john }.each_with_index do |user, index|
-  friendship = joe.sent_pending_requests.build(sender: joe, receiver: user)
+users.reject { |u| u == foo || u == john }.each_with_index do |user, index|
+  friendship = foo.sent_pending_requests.build(sender: foo, receiver: user)
   friendship.save
-  notification = joe.sent_notifications.build(receiver: user,
+  notification = foo.sent_notifications.build(receiver: user,
                                               object_type: 'Friendship',
                                               description: 'new friend request',
                                               time_sent: (Time.zone.now + index).to_s,
@@ -24,9 +24,9 @@ users.reject { |u| u == joe || u == john }.each_with_index do |user, index|
   notification.save
 end
 
-friendship = john.sent_pending_requests.build(sender: john, receiver: joe)
+friendship = john.sent_pending_requests.build(sender: john, receiver: foo)
 friendship.save
-notification = john.sent_notifications.build(receiver: joe,
+notification = john.sent_notifications.build(receiver: foo,
                                              object_type: 'Friendship',
                                              description: 'new friend request',
                                              time_sent: Time.zone.now.to_s,
