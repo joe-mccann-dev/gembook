05467a5c49c66ab2e3990618696209111808c302
Tenacious-Qi <joe.mccann@mailbox.org>
2022-01-20 01:04:01 -0500

    Display profile pictures next to user names. Add default profile picture. Write a display profile icons spec.

---
+++ spec/system/display_profile_icons_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "DisplayProfileIcons", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  let!(:user) { User.create(first_name: 'abcde', last_name: 'wxzy', email: 'abcde@wxzy.com', password: 'foobar') }
+  let!(:other_user) { User.create(first_name: 'george', last_name: 'washington', email: 'george@washington.com', password: 'foobar') }
+
+  context 'a user is logged in at the users#index page' do
+    before do
+      Capybara.current_driver = Capybara.javascript_driver
+      login_as(other_user, scope: :user)
+      visit new_user_profile_path(other_user)
+      image_1_file_path = "#{Rails.root}/spec/files/image_1.jpg"
+      attach_file(image_1_file_path)
+      click_on "Create Profile"
+      click_link 'Sign out'
+    end
+
+    it "shows them users names with profile pictures next to them" do
+      login_as(user, scope: :user)
+      visit users_path
+      click_on "Show Other Users"
+      expect(page).to have_css("img[src*='image_1.jpg']")
+    end
+  end
+end
