8475e437b00a0b5dbe6ec717ff854e4e79a3c84d
Tenacious-Qi <joe.mccann@mailbox.org>
2022-02-10 02:23:38 -0500

    Order notifications in descending order.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.received_notifications.includes([:sender])
+    @notifications = current_user.received_notifications.includes([:sender]).order(created_at: :desc)
     @friendships = current_user.requests_via_notification_sender_id
   end
 
