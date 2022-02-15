7081a69d443db547ec498a16b5f6caebb82fb382
Tenacious-Qi <joe.mccann@mailbox.org>
2022-02-15 00:59:32 -0500

    Update placeholders.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,11 +2,9 @@ module PostsHelper
 
   def placeholders
     [
-      "All that is gold does not glitter. Not all those who wander are lost. - J.R.R. Tolkien",
-      "It's hard to be a bright light in a dim world. - Gary Starta",
+      "What's new?",
       "To shine your brightest light is to be who you truly are. - Roy T. Bennett",
       "Shine on you crazy diamond. - Pink Floyd",
-      "We've all got both light and dark inside us. What matters is the part we choose to act on. - Harry Potter and the Prisoner of Azkaban"
     ]
   end
 
