28f276693944ee6e860e4db797a96f5c8b24e55e
Joseph McCann <joe.mccann@mailbox.org>
2022-11-27 13:48:26 -0500

    Remove forward slash from hostname.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.force_ssl = true
 
     # default host for production
-  host = 'https://gembook.onrender.com/'
+  host = 'https://gembook.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
