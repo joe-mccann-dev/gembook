7e4228d8a757985ff14b61c37693b97ed6dc19ed
Joe McCann <joe.mccann@mailbox.org>
2023-09-26 02:11:42 -0400

    Remove placeholders. Add sparkle emoji to users header.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,4 @@
 module PostsHelper
-
-  def placeholders
-    [
-      "What's new?",
-      "To shine your brightest light is to be who you truly are. - Roy T. Bennett",
-      "Shine on you crazy diamond. - Pink Floyd",
-    ]
-  end
-
   def abbreviated(post)
     post.content.split[0..20].join(' ')
   end
